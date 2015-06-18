665cc5afe0ade197b725b2844d2263d97e470df9
Giannis Dem <giannidis50@hotmail.com>
2015-06-18 15:12:21 +0300

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,11 @@ User.create!(name:  "giannikos",
              email: "giannidis50@hotmail.com",
              password:              "12345678",
              password_confirmation: "12345678",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+
 
 99.times do |n|
   name  = Faker::Name.name
@@ -11,5 +15,7 @@ User.create!(name:  "giannikos",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
