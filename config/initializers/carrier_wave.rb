6e2b591920a7d9587b3853592883db4d5457c534
Giannis Dem <giannidis50@hotmail.com>
2015-06-26 20:33:10 +0300

    trying to fix s3 frankfurt problem at1

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       :aws_access_key_id      => ENV['S3_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET'],
       :endpoint               => "https://s3.amazonaws.com",
-      :region                 => ENV['S3_REGION'] 
+      :region                 => ENV['eu-central-1'] 
   }
   config.fog_directory  = ENV['S3_BUCKET']
   end
