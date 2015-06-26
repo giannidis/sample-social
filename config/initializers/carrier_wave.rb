55a2e37e9039c1ea0d67a244bcfacc8784938263
Giannis Dem <giannidis50@hotmail.com>
2015-06-26 20:36:37 +0300

    at2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider               => 'AWS4-HMAC-SHA256',
+      :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET'],
       :endpoint               => "https://s3.amazonaws.com",
