a3d1b22a8b36397e126d43eb74f54f37b109e544
unknown <giannidis50@hotmail.com>
2015-07-01 13:42:56 +0300

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,11 @@ User.create!(name:  "giannikos",
               activated_at: Time.zone.now)
 end
 
+#microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
+
+
 end
